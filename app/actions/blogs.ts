d3c7e46b6ea0277868896dada0380245e16efa30
PermataAyu <unocondo@gmail.com>
2026-06-24 13:07:43 +0700

    exercise 4

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -1,7 +1,7 @@
 "use server"
 
 import {redirect} from "next/navigation"
-import {addBlog} from "../services/blogs"
+import {addBlog, likeBlog} from "../services/blogs"
 import { revalidatePath } from "next/cache"
 
 export const createBlog = async (formData: FormData) => {
@@ -11,4 +11,11 @@ export const createBlog = async (formData: FormData) => {
   addBlog(title, author, url)
   revalidatePath('/blogs')
   redirect('/blogs')
+}
+
+export const likingBlog = async (formData: FormData) => {
+  const id = Number(formData.get("id"))
+  likeBlog(id)
+  revalidatePath(`/blogs/${id}`)
+  revalidatePath('/blogs')
 }
--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { getBlogById } from "@/app/services/blogs"
+import { likingBlog } from "../../actions/blogs"
+import { getBlogById } from "../../services/blogs"
 import { notFound } from "next/navigation"
 
 const NotePage = async ({params}: {params: Promise<{id: string}>}) => {
@@ -14,7 +15,15 @@ const NotePage = async ({params}: {params: Promise<{id: string}>}) => {
       <h2>{blog.title}</h2> 
       <div>by {blog.author}</div>
       <div><a href={blog.url}>{blog.url}</a></div>
-      <div>{blog.likes} likes</div>
+      <div>
+        {blog.likes} likes
+        <form action={likingBlog}>
+          <input type="hidden" name="id" value={blog.id} />
+          <button type="submit">
+            Like
+          </button>
+        </form>
+      </div>
     </div>
   )
 
