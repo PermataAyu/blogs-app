15b8e43a138ba11fd5524ded92ab336936feda2a
PermataAyu <unocondo@gmail.com>
2026-06-29 11:27:20 +0700

    exercise 15

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -3,12 +3,30 @@
 import { db } from "@/db"
 import { users } from "@/db/schema"
 import bcrypt from "bcryptjs"
+import { eq } from "drizzle-orm"
 import { redirect } from "next/navigation"
 
-export const registerUser = async (formData: FormData) => {
+export const registerUser = async (
+  prevState: {errors: object, values: object},
+  formData: FormData
+) => {
+  const errors: {[key: string]: string} = {}
   const username = (formData.get("username") as string).trim()
   const name = (formData.get("name") as string).trim()
   const password = formData.get("password") as string
+  const passwordConfirm = formData.get("passwordconfirm") as string
+
+  if (password !== passwordConfirm) {
+    errors.password = "password doesnt match"
+  }
+  const findUser = await db.query.users.findFirst({where: eq(users.username, username)})
+  if (findUser) {
+    errors.username = "username already used"
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {errors, values: {username, name}}
+  }
 
   const passwordHash = await bcrypt.hash(password, 10)
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,26 +1,49 @@
+"use client"
+
+import { useActionState } from "react";
 import { registerUser } from "../actions/users";
 
 export default function RegisterPage() {
+  const initialState = {
+    errors: {
+      password: "",
+      username: ""
+    },
+    values: {
+      username: "",
+      name: ""
+    }
+  }
+
+  const [state, formAction] = useActionState(registerUser, initialState)
   return (
     <div>
       <h2>Register</h2>
-      <form action={registerUser}>
+      <form action={formAction}>
         <div>
           <label>
             Username
-            <input name="username" type="text" required/>
+            <input name="username" type="text" required defaultValue={state.values.username}/>
+            {state.errors.username && <p style={{color: "red"}}>{state.errors.username}</p>}
           </label>
         </div>
         <div>
           <label>
             Name
-            <input name="name" type="text" required/>
+            <input name="name" type="text" required defaultValue={state.values.name}/>
           </label>
         </div>
         <div>
           <label>
             Password
             <input name="password" type="password" required/>
+          </label>
+        </div>
+        <div>
+          <label>
+            Password Confirm
+            <input name="passwordconfirm" type="password" required/>
+            {state.errors.password && <p style={{color: "red"}}>{state.errors.password}</p>}
           </label>
         </div>
         <button type="submit">Create Account</button>
