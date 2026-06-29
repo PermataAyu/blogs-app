cccecfa3c270c0ef3c68813eb66c12080f14a48a
PermataAyu <unocondo@gmail.com>
2026-06-29 08:41:48 +0700

    exercise 13

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -5,7 +5,10 @@ import { addBlog, likeBlog } from "../services/blogs"
 import { revalidatePath } from "next/cache"
 import { auth } from "@/auth"
 
-export const createBlog = async (formData: FormData) => {
+export const createBlog = async (
+  prevState: {error: string},
+  formData: FormData
+) => {
   const session = await auth()
 
   if (!session) {
@@ -13,8 +16,18 @@ export const createBlog = async (formData: FormData) => {
   }
 
   const title = formData.get("title") as string
+
+  if ( !title || title.length < 5 ) {
+    return { error: "Title minimum 5 character long" }
+  }
   const author = formData.get("author") as string
+  if ( !author || author.length < 5 ) {
+    return {error: "Author minimum 5 character long"}
+  }
   const url = formData.get("url") as string
+  if ( !url || url.length < 5 ) {
+    return {error: "URL minimum 5 character long"}
+  }
   
   await addBlog(title, author, url)
   revalidatePath('/blogs')
